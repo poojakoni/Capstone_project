8c1f27b85cbee961c97b3f3487f270dbd8f70731
your name <you@example.com>
2023-04-05 11:42:43 +0530

    file name

--- src/test/java/StepDef/Signup_page_stepdef.java
+++ src/test/java/StepDef/Signup_page_stepdef.java
@@ -43,7 +43,7 @@ public class Signup_page_stepdef {
 		
 		// giving the email
 		WebElement emaliid = driver.findElement(By.xpath("//input[@id=\"email\"]"));
-		emaliid.sendKeys("ing@gmail.com");
+		emaliid.sendKeys("sdfcing@gmail.com");
 		
 		// giving the contact no
 		WebElement cno = driver.findElement(By.xpath("//input[@id=\"contactNumber\"]"));
